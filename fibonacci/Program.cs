bd5d59a6387232eea7993425cfd96002219fab82
marcotriesdev <50000tons@gmail.com>
2024-12-05 13:50:26 -0600

    agregado generar fibonacci

---
+++ fibonacci/Program.cs
@@ -0,0 +1,34 @@
+﻿
+using System;
+
+namespace fibonacci
+{
+	class Fibonacci
+	{
+		static void Main(string[] args)
+		{
+			int limite;
+			Console.WriteLine("FIBONACCI");
+			Console.WriteLine("Ingrese un número para generar hasta ese numero la serie fibonacci:");
+
+			limite = int.Parse(Console.ReadLine());
+
+			Console.WriteLine("Secuencia Fibonacci: ");
+			int num1 = 0;
+			int num2 = 1;
+			Console.WriteLine($"{num1}");
+			Console.WriteLine($"{num2}");
+
+			for (int i = 3; i <= limite; i++){
+
+				int next = num1 + num2;
+				Console.WriteLine($"{next}");
+				num1 = num2;
+				num2 = next;
+
+			}
+			
+		}
+	}
+}
+
