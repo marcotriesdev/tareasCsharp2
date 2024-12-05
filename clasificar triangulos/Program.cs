4d8456f637f737499e53921d2750e909ef8c050c
marcotriesdev <50000tons@gmail.com>
2024-12-05 09:47:36 -0600

    Commit inicial tarea 2

---
+++ clasificar triangulos/Program.cs
@@ -0,0 +1,63 @@
+﻿
+using System;
+
+namespace clasificarTriangulos
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			float side1;
+			float side2;
+			float side3;
+
+			Console.WriteLine("CLASIFICACION DE TRIANGULOS");
+			Console.WriteLine("Ingresa los 3 lados de tu triángulo");
+
+			side1 = float.Parse(Console.ReadLine());
+			side2 = float.Parse(Console.ReadLine());
+			side3 = float.Parse(Console.ReadLine());
+
+		if (esTrianguloValido(side1, side2, side3))
+		{
+			// Determinar el tipo de triángulo
+			string tipo = tipoTriangulo(side1, side2, side3);
+			Console.WriteLine($"El triángulo es {tipo}.");
+		}
+		else
+		{
+			Console.WriteLine("Las longitudes no forman un triángulo válido.");
+		}
+
+
+
+		}
+
+
+	static string tipoTriangulo(double a, double b, double c)
+	{
+		// Verificar el tipo de triángulo
+		if (a == b && b == c)
+		{
+			return "equilátero";
+		}
+		else if (a == b || a == c || b == c)
+		{
+			return "isósceles";
+		}
+		else
+		{
+			return "escaleno";
+		}
+	}
+
+	static bool esTrianguloValido(double a, double b, double c)
+	{
+		// Condición de existencia de un triángulo
+		return a + b > c && a + c > b && b + c > a;
+	}
+			
+		
+	}
+}
+
