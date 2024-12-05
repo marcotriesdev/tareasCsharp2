9d6cde3d97d1c566fe5e66e61b45d17189009409
marcotriesdev <50000tons@gmail.com>
2024-12-05 10:58:06 -0600

    agregado calificaciones

---
+++ calificaciones/Program.cs
@@ -0,0 +1,68 @@
+﻿
+
+using System;
+using System.Diagnostics.Contracts;
+
+namespace Calificaciones
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int punteo;
+			char calificacion;
+
+			Console.WriteLine("CALIFICACIONES");
+			Console.WriteLine("Ingrese su calificación obtenida del 1 al 100: ");
+			punteo = int.Parse(Console.ReadLine());
+
+			if (punteo <= 20){
+				calificacion = 'F';
+				Calificar(calificacion);
+			}
+			else if (punteo > 20 && punteo <= 40){
+				calificacion = 'D';
+				Calificar(calificacion);
+			}
+			else if (punteo > 40 && punteo <= 60){
+				calificacion = 'C';
+				Calificar(calificacion);
+			}
+			else if (punteo > 60 && punteo <= 80){
+				calificacion = 'B';
+				Calificar(calificacion);
+			}
+			else if (punteo > 80 && punteo <= 100){
+				calificacion = 'A';
+				Calificar(calificacion);
+			}
+
+
+			static void Calificar(char calificacion){
+
+			
+			switch (calificacion){
+				case 'A':
+					Console.WriteLine($"Usted sacó excelente calificación: {calificacion}");
+					break;
+				case 'B':
+					Console.WriteLine($"Su calificación es muy buena: {calificacion}");
+					break;
+				case 'C':
+					Console.WriteLine($"Su calificación es buena: {calificacion}");
+					break;
+				case 'D':
+					Console.WriteLine($"Su calificación es regular: {calificacion}");
+					break;
+				case 'F':
+					Console.WriteLine($"Usted perdió la clase con calificación: {calificacion}");
+					break;		
+				default:
+					Console.WriteLine("No tiene calificación");
+					break;								
+			}
+			}
+
+		}
+	}
+}
